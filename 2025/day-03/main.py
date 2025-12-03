ea716915433759533b3d9b638fc4981df9cd0289
Gael Melo <gaelmmelo@gmail.com>
2025-12-03 13:16:12 -0700

    Improve day 03 part 1

--- 2025/day-03/main.py
+++ 2025/day-03/main.py
@@ -1,9 +1,11 @@
 def main():
     with open('input.txt', 'r') as file:
         total_joltage = 0
-        for line in file: total_joltage += pack_joltage(line.strip())
-        print("The provided batteries can provide you "
-              + f"with a total of {total_joltage} jolts.")
+        for line in file:
+            line = line.strip()
+            if not line: continue
+            total_joltage += pack_joltage(line)
+        print(f"The provided batteries can provide you with a total of {total_joltage} jolts.")
 
 # The joltage of a pack is the maximum number that can be formed
 # by combining the joltage digit of two batteries in the pack
@@ -11,7 +13,7 @@ def main():
 def pack_joltage(pack: str) -> int:
     joltages = [int(x) for x in pack]
     left_joltage = max(joltages[:-1])
-    left_index = pack.find(str(left_joltage))
+    left_index = joltages.index(left_joltage)
     right_joltage = max(joltages[left_index+1:])
     return left_joltage * 10 + right_joltage
 
